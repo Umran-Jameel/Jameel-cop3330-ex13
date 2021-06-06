0e09291e5b25e32b2db99a8cbe94c8ffbb81774a
Umran Jameel <umranrazack02@mak-bwk-lmran.local>
2021-06-06 14:53:24 -0400

    Initial commit

---
+++ src/main/java/Exercise13.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Exercise13 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the principal amount? ");
+        String prin = in.nextLine();
+        double principal = Double.parseDouble(prin);
+
+        System.out.print("What is the rate? ");
+        String ra = in.nextLine();
+        double rate = Double.parseDouble(ra);
+        rate /= 100;
+
+        System.out.print("What is the number of years? ");
+        String year = in.nextLine();
+        double years = Double.parseDouble(year);
+
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        String ti = in.nextLine();
+        double times = Double.parseDouble(ti);
+
+        double exponent = times * years;
+        double total = principal * (Math.pow((1 + (rate / times)), exponent));
+
+        System.out.printf("$%.2f invested at %.2f for %.0f years compunded %.0f times per year is %.2f.", principal, rate * 100, years, times, total);
+    }
+}
